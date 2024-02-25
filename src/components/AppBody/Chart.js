6d381bb583421657847f910c843e6b7f8b791527
Waldemar Glaz <waldekglaz@gmail.com>
2024-02-25 17:53:25 +0100

    highest amout dynamically styled

--- src/components/AppBody/Chart.js
+++ src/components/AppBody/Chart.js
@@ -2,11 +2,16 @@ import React from "react";
 import { StyledChart } from "./Chart.styled";
 import { expenses } from "../../data";
 import { useState } from "react";
+const highestAmount = expenses.reduce(
+  (max, obj) => (obj.amount > max ? obj.amount : max),
+  -Infinity
+);
 const Chart = () => {
   const [isActive, setIsActive] = useState(false);
   const clickHandler = (e) => {
     setIsActive((current) => !current);
   };
+
   return (
     <StyledChart>
       {expenses.map((data, key) => {
@@ -18,7 +23,8 @@ const Chart = () => {
             <div
               style={{
                 height: data.amount * 3 + "px",
-                backgroundColor: data.amount > 51 ? "#76B5BC" : "#ec755d",
+                backgroundColor:
+                  data.amount === highestAmount ? "#76B5BC" : "#ec755d",
               }}
               className="column"
               data-expense={data.amount}
